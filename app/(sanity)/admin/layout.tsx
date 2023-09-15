80c066327b99db6884265dc1948957b67944b1e2
maksim hodasevich <maksim.hodasevich@gmail.com>
2023-09-15 07:39:16 +0200

    feat: add noindex meta to admin page

--- app/(sanity)/admin/layout.tsx
+++ app/(sanity)/admin/layout.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from 'react';
 
+export const metadata = {
+  robots: 'noindex',
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
