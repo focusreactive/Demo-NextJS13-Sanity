c20a47a196de95c4bbeb4380f026cf7db71c11f4
Eugene Boruhov <eugene@focusreactive.com>
2023-09-25 12:14:33 +0200

    Enable preview only for `Dynamic Pages`

--- sanity/deskStructure.ts
+++ sanity/deskStructure.ts
@@ -1,9 +1,17 @@
-import { StructureBuilder, StructureResolver } from 'sanity/desk';
+import { DefaultDocumentNodeContext, StructureBuilder, StructureResolver } from 'sanity/desk';
 import { PreviewIFrame } from '@/sanity/preview/Preview';
+import { dynamicPage } from '@/sanity/schemas/dynamicPage';
 
 export const deskStructure: StructureResolver = (S) => {
   return S.defaults();
 };
 
-export const defaultDocumentNode = (S: StructureBuilder) =>
-  S.document().views([S.view.form(), S.view.component(PreviewIFrame).title('Preview')]);
+export const defaultDocumentNode = (S: StructureBuilder, { schemaType }: DefaultDocumentNodeContext) => {
+  const documentsWithPreview: string[] = [dynamicPage.name];
+
+  if (documentsWithPreview.includes(schemaType)) {
+    return S.document().views([S.view.form(), S.view.component(PreviewIFrame).title('Preview')]);
+  }
+
+  return S.document();
+};
