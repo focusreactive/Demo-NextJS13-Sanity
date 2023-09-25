9a65ab34690d07c36f875258e602ba85b9b994eb
Eugene Boruhov <eugene@focusreactive.com>
2023-09-25 15:31:24 +0200

    Add api token to Sanity client

---
+++ environment.ts
@@ -0,0 +1,2 @@
+export const sanityApiToken = process.env.SANITY_API_TOKEN as string;
+
--- sanity/client.ts
+++ sanity/client.ts
@@ -1,5 +1,9 @@
 import { createClient } from 'next-sanity';
 
 import config from './config';
+import { sanityApiToken } from '@/environment';
 
-export const client = createClient(config);
+export const client = createClient({
+  ...config,
+  token: sanityApiToken,
+});
