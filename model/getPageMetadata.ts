542c1e80bef0371246755f34be796665f3f17985
Eugene Boruhov <eugene@focusreactive.com>
2023-10-09 21:46:04 +0200

    Fix error in `generateMetadata`

--- model/getPageMetadata.ts
+++ model/getPageMetadata.ts
@@ -2,17 +2,11 @@ import { PAGE_METADATA_QUERY } from '@/model/queries';
 import { client } from '@/sanity/client';
 
 export const getPageMetadata = async ({ slug }: { slug: string }) => {
-  try {
-    return await client.fetch(
-      PAGE_METADATA_QUERY,
-      {
-        slug,
-      },
-      { cache: 'no-cache' },
-    );
-  } catch (error) {
-    console.error(error);
-  }
-
-  return null;
+  return await client.fetch(
+    PAGE_METADATA_QUERY,
+    {
+      slug,
+    },
+    { cache: 'no-cache' },
+  );
 };
