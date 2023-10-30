f4754cdce60143187a1d1e46e4a4778876230bf0
Eugene Boruhov <eugene@focusreactive.com>
2023-10-30 12:08:20 +0100

    Disable visual editing

--- sanity/config.ts
+++ sanity/config.ts
@@ -3,8 +3,6 @@ const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false,
   apiVersion: '2021-10-21',
-  studioUrl: '/admin',
-  logger: console,
 };
 
 export default config;
