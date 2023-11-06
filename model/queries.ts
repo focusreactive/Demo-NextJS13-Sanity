149037014cf933978e26d49552188ce23c931535
Oleg Proskurin <regx@usul.su>
2023-11-06 22:44:17 +0700

    Add ref resolver for Capabilities

--- model/queries.ts
+++ model/queries.ts
@@ -11,7 +11,22 @@ export const ALL_PAGES_SLUGS_QUERY = groq`
 export const PAGE_CONTENT_QUERY = groq`
     * [_type == 'dynamicPage' && !(_id in path('drafts.**')) && slug.current == $slug] {
       ${headerAndFooter}
-      content[]
+      content[]{
+        ...,
+        _type == 'capabilities' => {
+          ...,
+          list[]{
+            ...,
+            button{
+              ...,
+              uri[]{
+                ...,
+                'ref': reference->
+              }
+            }
+          }
+        }
+      }
     }[0]
   `;
 
