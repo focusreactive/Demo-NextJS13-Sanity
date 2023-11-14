580c7ce48a68eb9119305693df538619caa6f1e1
Eugene Boruhov <eugene@focusreactive.com>
2023-11-14 13:45:47 +0100

    Update test

--- focus-reactive/sanity-overlay/test.spec.ts
+++ focus-reactive/sanity-overlay/test.spec.ts
@@ -1,14 +1,17 @@
 import { patchStringFields } from './src/enableVisualEditing';
 import { expect, it } from '@jest/globals';
-import * as fs from 'fs';
 
 import page from './fixtures/page.json';
 import patchedPage from './fixtures/patchedPage.json';
+import * as fs from 'fs';
 
-const mark = '+++@+++';
+it(`should add stega to each text field`, () => {
+  patchStringFields(page, {
+    patchCb: (value, { id, type, path }) => `${id}__${type}__${path}`,
+    excludedPaths: [/.*footer\.socials\[\d+\]\.icon.*/],
+  });
 
-it(`should add ${mark} to each text field`, () => {
-  patchStringFields(page, (value, path) => `${value}${mark}`, [/.*footer\.socials\[\d+\]\.icon.*/]);
+  fs.writeFileSync('./page.json', JSON.stringify(page, null, 2));
 
   expect(page).toEqual(patchedPage);
 });
