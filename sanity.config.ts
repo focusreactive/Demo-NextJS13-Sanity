cf644a080e6f665e745706932375e69448cad523
Alex Hramovich <alex.gramovich.1996@gmail.com>
2024-01-16 10:11:05 +0100

    add ai toolkit plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { colorInput } from '@sanity/color-input';
 import { media } from 'sanity-plugin-media';
+import { aiToolkit } from '@focus-reactive/sanity-ai-toolkit';
 
 import { schemaTypes } from '@/sanity/schemas';
 import config from '@/sanity/config';
@@ -24,6 +25,14 @@ export default defineConfig({
     visionTool(),
     media(),
     colorInput(),
+    aiToolkit({
+      openAiToken: process.env.SANITY_STUDIO_OPENAI_TOKEN as string,
+      featuresConfig: {
+        translate: { enabled: true },
+        summary: { enabled: true },
+        tags: { enabled: true },
+      },
+    }),
   ],
 
   schema: {
